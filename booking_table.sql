baf5c2e9f851436ae80cf60f80d4b22ac4b831dd
gobuk <borneogods@pm.me>
2024-11-01 07:40:54 +0800

    Add booking table schema file

---
+++ booking_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE booking (
+booking_no INT(12) KEY NOT NULL,
+booking_date DATE,
+bill_instruction VARCHAR(50),
+guest_name VARCHAR(15),
+booking_status VARCHAR(20),
+travel_agent VARCHAR(50),
+ota_no VARCHAR(24));
